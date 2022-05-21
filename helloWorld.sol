dc250b76b6d8c65cab6f8204cd8e5283e55096cd
Arnav2300 <56104491+Arnav2300@users.noreply.github.com>
2022-05-21 19:38:17 +0530

    Create helloWorld.sol

---
+++ helloWorld.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+contract helloWorld{
+  string public str="hello world";
+}
+
+/*
+  can run without license but will give a warning
+  best practice to put in a specific solidity version
+  >=0.8.7 is written like ^0.8.7
+  compile the contract on remix
+  deploy it
+  call the variable in the ide
+  this smart contract runs on a local blockchain in the browser
+*/
